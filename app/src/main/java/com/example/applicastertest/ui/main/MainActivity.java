151beab987ca373f25e065af87fb80b6b6905be2
Evin1- <edwin.hdz.12.0@gmail.com>
2017-03-28 22:07:59 -0400

    Add icons

--- app/src/main/java/com/example/applicastertest/ui/main/MainActivity.java
+++ app/src/main/java/com/example/applicastertest/ui/main/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
+        if (id == R.id.m_search) {
             return true;
         }
 
