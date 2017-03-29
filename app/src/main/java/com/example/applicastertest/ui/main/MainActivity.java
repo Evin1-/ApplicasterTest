0f000bdb68733ffb0eab76710b98043ad4596d15
Evin1- <edwin.hdz.12.0@gmail.com>
2017-03-28 22:03:39 -0400

    Rename ids and remove unused drawer buttons

--- app/src/main/java/com/example/applicastertest/ui/main/MainActivity.java
+++ app/src/main/java/com/example/applicastertest/ui/main/MainActivity.java
@@ -1,10 +1,6 @@
-package com.example.applicastertest;
+package com.example.applicastertest.ui.main;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,16 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.applicastertest.R;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.drawer_layout)
+    @BindView(R.id.a_main_drawer)
     DrawerLayout drawerLayout;
-    @BindView(R.id.nav_view)
+    @BindView(R.id.a_main_nav)
     NavigationView navigationView;
-    @BindView(R.id.toolbar)
+    @BindView(R.id.a_main_toolbar)
     Toolbar toolbar;
 
     @Override
@@ -62,14 +60,6 @@ public class MainActivity extends AppCompatActivity {
             if (id == R.id.nav_camera) {
 
             } else if (id == R.id.nav_gallery) {
-
-            } else if (id == R.id.nav_slideshow) {
-
-            } else if (id == R.id.nav_manage) {
-
-            } else if (id == R.id.nav_share) {
-
-            } else if (id == R.id.nav_send) {
 
             }
 
